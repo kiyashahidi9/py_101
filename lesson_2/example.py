d3e55121eba11b3874f168e10d6907115b1d3883
Kiya Shahidi <kiyashahidi9@gmail.com>
2024-09-12 18:52:09 +0000

    changes to rock paper scissors

--- lesson_2/example.py
+++ lesson_2/example.py
@@ -1,6 +1,4 @@
-words = ['scooby', 'do', 'on', 'channel', 'two']
-
-for word in words:
-    words.pop(0)
-
-print(words)  # prints: ['do', 'channel']
+if []:
+    print('truthy')
+else:
+    print('falsy')
--- lesson_2/rock_paper_scissors.py
+++ lesson_2/rock_paper_scissors.py
@@ -1,5 +1,3 @@
-# 
-
 # importing for computer choice
 import random
 
@@ -7,7 +5,7 @@ import random
 VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
 # returns a list to display choices with () around short choices
-def return_choices_list(choice_list):
+def return_choices_list():
     display_list = []
 
     for option in VALID_CHOICES:
@@ -19,11 +17,11 @@ def return_choices_list(choice_list):
             option = f'({short}){option[2:]}'
 
         display_list.append(option)
-    
-    return (', '.join(display_list))
+
+    return ', '.join(display_list)
 
 # creates a list of valid short choices
-def shortened_choices(choice_list):
+def shortened_choices():
     shortened_list = []
 
     for option in VALID_CHOICES:
@@ -33,45 +31,43 @@ def shortened_choices(choice_list):
             short = option[0:2]
 
         shortened_list.append(short)
-    
-    return (shortened_list)
 
+    return shortened_list
 # if given a shortened input, converts to full word
+
 def process_shortened_input(short_input):
     for word in VALID_CHOICES:
         if word.startswith(short_input):
             return word
-    
+
 # default print format
 def prompt(message):
     print(f'==> {message}')
 
 # checks choice for valid input
 def invalid_choice(player_choice):
-    return (player_choice.casefold() not in VALID_CHOICES and 
+    return (player_choice.casefold() not in VALID_CHOICES and
             player_choice.casefold() not in valid_shortened_choices)
 
 # computes if player wins
 def player_won(player, computer):
-    if ((player == 'rock' and (computer in 
+    return ((player == 'rock' and (computer in
                                 ['scissors', 'lizard'])) or
-        (player == 'scissors' and (computer in 
+        (player == 'scissors' and (computer in
                                 ['paper', 'lizard'])) or
-        (player == 'paper' and (computer in 
+        (player == 'paper' and (computer in
                                 ['rock', 'spock'])) or
-        (player == 'lizard' and (computer in 
-                                ['spock', 'paper'])) or 
-        (player == 'spock' and (computer in 
+        (player == 'lizard' and (computer in
+                                ['spock', 'paper'])) or
+        (player == 'spock' and (computer in
                                 ['scissors', 'rock']))
-    ):
-        return True
-    else:
-        return False            
+    )
 
 # checks play_again for valid input
 def invalid_again(again_choice):
     return not(again_choice.startswith('n') or again_choice.startswith('y'))
 
+# displays total wins
 def display_total_wins():
     print(f'''
 You have {player_wins} wins!
@@ -79,8 +75,8 @@ The computer has {computer_wins} wins
            ''')
 
 # more variables
-display_choices = return_choices_list(VALID_CHOICES)
-valid_shortened_choices = shortened_choices(VALID_CHOICES)
+display_choices = return_choices_list()
+valid_shortened_choices = shortened_choices()
 
 while True:
 
@@ -124,10 +120,10 @@ while True:
         if player_wins == 3:
             prompt('You won the game!')
             break
-        elif computer_wins == 3:
+        if computer_wins == 3:
             prompt('You lost the game :(')
             break
-            
+
     # asks user to play again and validates input
     prompt('Would you like to play again? (y/n)')
     play_again = input().lower()
@@ -135,7 +131,7 @@ while True:
     while invalid_again(play_again):
         prompt('Please enter a valid choice (y/n)')
         play_again = input().lower()
-    
+
     if play_again[0].lower() == 'n':
         break
 
